6a31e59f84daecbaa672bd30dfef097bb25072fc
Olli Wilkman <olli.wilkman@iki.fi>
2021-09-01 17:35:35 +0300

    Update for Pipenv

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 
 # HSL API test
 
-This is a simple Python script that takes a list of stop names from the command line,
+This is a simple Python script that takes a number and a list of stop names from the command line,
 asks the HSL API for the next five departures from those stops, and outputs a bare-boned
 HTML page with the departures organized in a table.
+
+## Setup
+
+Easiest would be with [Pipenv](https://pipenv.pypa.io/en/latest/).
+Run `pipenv install` to install the dependencies from the `Pipfile`
+and create a virtual environment for running the code.
+
+But right now the only dependency is [requests](https://docs.python-requests.org/en/master/)
+so you can just install that any other way and run the code.
+
+Requires Python 3.6 or higher, I think. Only tested on 3.9 right now.
 
 ## Example
 
 The file `example.html` in this repo was produced with the command line
 
 ```
-$ python timetable.py Kalervonkatu Sumatrantie > example.html
+pipenv run python timetable.py 5 Kalervonkatu Sumatrantie > example.html
 ```
+
+It prints out the HTML page with the next 5 departures from these two stops combined.
--- timetable.py
+++ timetable.py
@@ -1,22 +1,54 @@
 import requests
-from pprint import pprint
+import logging
+from pprint import pformat
 from string import Template
 from sys import argv
 from datetime import datetime, timedelta
+from itertools import islice
 
 from html_templates import HTML_HEADER, HTML_FOOTER, HTML_ROW_TEMPLATE
 REQUEST_TEMPLATE = Template(open("request_template.txt", "r").read())
 URL = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
 UTC_OFFSET = timedelta(hours=3)
 
-def get_stop_departures(stopID, N=1):
-    "Get the next N departures from given stop as JSON."
-    data = REQUEST_TEMPLATE.substitute(ID=stopID, N=N)
-    r = requests.post(URL, headers={"Content-Type" : "application/graphql"}, data=data)
+logging.basicConfig(level=logging.WARN)
+
+def get_stop_departures(name, N=1):
+    """HTTP request for the next N departures from given stop.
+    """
+    query = REQUEST_TEMPLATE.substitute(ID=name, N=N)
+    logging.debug(f"Making GraphQL query:\n{query}")
+    r = requests.post(URL, headers={"Content-Type" : "application/graphql"}, data=query)
     return r.json()
 
+
+def parse_departures(data):
+    """Parse response JSON into (timestamp, HTML string) pairs.
+    """
+    departure_list = data["data"]["stops"][0]["stoptimesWithoutPatterns"]
+    return [parse_html(json) for json in departure_list]
+
+
+def parse_html(json):
+    """Make HTML string out of departure JSON.
+    """
+    scheduled, realtime = get_timestamp(json)
+    name = json["trip"]["route"]["shortName"]
+    headsign = json["headsign"]
+    stop = json["stop"]["name"]
+    time = scheduled.strftime("%H:%M")
+    # Add note if the bus is more than a minute late
+    if abs(scheduled - realtime) > timedelta(minutes=1):
+        note = "Estimated: {}".format(realtime.strftime("%H:%M"))
+    else:
+        note = ""
+    return scheduled, HTML_ROW_TEMPLATE.substitute(time=time, name=name, stop=stop,
+                                                   headsign=headsign, note=note)
+
+
 def get_timestamp(departure):
-    "Get scheduled/realtime departure datetimes from JSON."
+    """Get scheduled/realtime departure datetimes from JSON.
+    """
     day_start_unix = departure['serviceDay']
     scheduled_time = departure['scheduledDeparture']
     real_time = departure['realtimeDeparture']
@@ -24,35 +56,21 @@ def get_timestamp(departure):
     datetime_realtime = datetime.utcfromtimestamp(day_start_unix + real_time)
     return datetime_scheduled + UTC_OFFSET, datetime_realtime + UTC_OFFSET
 
-def parse_departures(data):
-    "Parse response JSON into (timestamp, HTML string) pairs."
-    departure_list = data["data"]["stops"][0]["stoptimesWithoutPatterns"]
-    return [parse_html(json) for json in departure_list]
 
-def parse_html(json):
-    "Make HTML string out of departure JSON."
-    scheduled, realtime = get_timestamp(json)
-    name = json["trip"]["route"]["shortName"]
-    headsign = json["headsign"]
-    stop = json["stop"]["name"]
-    time = scheduled.strftime("%H:%M")
-    if abs(scheduled - realtime) > timedelta(minutes=1):
-        note = "Estimated: {}".format(realtime.strftime("%H:%M"))
-    else:
-        note = ""
-    return scheduled, HTML_ROW_TEMPLATE.substitute(time=time, name=name, stop=stop,
-                                                   headsign=headsign, note=note)
-    
 def main():
-    stops = argv[1:]
+    N = int(argv[1])
+    stops = argv[2:]
     print(HTML_HEADER)
     
     Departures = []
     for stop_name in stops:
-        data = get_stop_departures(stop_name, 5)
-        Departures += parse_departures(data)
-    for timestamp, html in sorted(Departures):
+        raw_data = get_stop_departures(stop_name, N)
+        logging.debug(f"Received response:\n{pformat(raw_data)}")
+        Departures += parse_departures(raw_data)
+    
+    for timestamp, html in islice(sorted(Departures), N):
         print(html)
+
     print(HTML_FOOTER)
 
 if __name__=="__main__":
